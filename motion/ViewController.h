03ab11c70f548662c76c8cc0bdae817294de5719
mayumii <supercell135@gmail.com>
2015-05-23 23:56:23 +0900

    Initial Commit

---
+++ motion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  motion
+//
+//  Created by mayumi on 2015/05/23.
+//  Copyright (c) 2015年 mayumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ motion/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  motion
+//
+//  Created by mayumi on 2015/05/23.
+//  Copyright (c) 2015年 mayumi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
